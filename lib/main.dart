37ad48b8fed8c26aafd97b92521717b40abb3c3d
kimilguk <kimilguk@knou.ac.kr>
2025-09-04 14:44:27 +0900

    9주차 1교시 강의 소스

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(KakaoMapPage());
+}
+
+class KakaoMapPage extends StatefulWidget {
+  const KakaoMapPage({super.key});
+
+  @override
+  State<KakaoMapPage> createState() => _KakaoMapPageState();
+}
+
+class _KakaoMapPageState extends State<KakaoMapPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
