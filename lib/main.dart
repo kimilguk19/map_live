98cecb41df22aa0c862bcfee97a70a4474d1e872
kimilguk <kimilguk@knou.ac.kr>
2025-09-05 16:50:29 +0900

    9주차 2교시 강의 소스

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kakao_map_sdk/kakao_map_sdk.dart'; // 외부 패키지 추가
 
 void main() {
   runApp(KakaoMapPage());
@@ -14,6 +15,20 @@ class KakaoMapPage extends StatefulWidget {
 class _KakaoMapPageState extends State<KakaoMapPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('카카오맵 예제')),
+        body: KakaoMap(
+          option: const KakaoMapOption(
+            position: LatLng(37.566535, 126.977969), // 서울 시청을 중심으로 지도 표시
+            zoomLevel: 16,
+            mapType: MapType.normal,
+          ),
+          onMapReady: (KakaoMapController controller) {
+            print("카카오 지도가 정상적으로 불러와졌습니다.");
+          },
+        ),
+      ),
+    );
   }
 }
