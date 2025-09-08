75c0bf4c90e4a2e15b229e8b2cb1fe3065e19d37
kimilguk <kimilguk@knou.ac.kr>
2025-09-08 13:58:20 +0900

    10주차 1교시 강의 소스1

---
+++ lib/api_service.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+// API 응답과 매칭될 클래스
+class ApiData {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  ApiData({
+    required this.userId, required this.id,
+    required this.title,
+    required this.body,
+  });
+
+  // JSON에서 ApiData 객체로 변환하는 팩토리 생성자
+  factory ApiData.fromJson(Map<String, dynamic> json) {
+    return ApiData(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+}
+
+// API 서비스를 담당하는 클래스
+class ApiService {
+  // API의 기본 URL
+  final String _baseUrl = 'https://apis.data.go.kr/5690000/sjElectricWheelchairFastCharger/sj_00000860?serviceKey=PLJPmKeBFGOkoxgAoLJgT962Uh0QPWijxPNQ%2Bl%2B4o24r9R%2BqbclT0Fc9xSamDrGiMYAF4CrpJLaDOsKZ%2FDoN%2Bw%3D%3D&pageIndex=1&pageUnit=20&dataTy=json&searchCondition=fclty_Nm';
+
+  // 특정 ID의 데이터를 가져오는 메소드 우리수업에서 사용하지 않기때문에 삭제
+  Future<ApiData> fetchData(int id) async {
+    final response = await http.get(Uri.parse('$_baseUrl/posts/$id'));
+
+    if (response.statusCode == 200) {
+      // 요청이 성공하면 JSON을 파싱합니다.
+      return ApiData.fromJson(jsonDecode(response.body));
+    } else {
+      // 요청이 실패하면 예외를 발생시킵니다.
+      throw Exception('Failed to load data');
+    }
+  }
+
+  // 모든 데이터를 리스트 형태로 가져오는 메소드 (우리수업에서는 $_baseUrl만 사용하기 때문에 수정)
+  Future<List<ApiData>> fetchAllData() async {
+    final response = await http.get(Uri.parse('$_baseUrl/posts')); // /posts 제거
+
+    if (response.statusCode == 200) {
+      // JSON 배열을 파싱합니다.
+      List<dynamic> body = jsonDecode(response.body);
+      // 각 JSON 객체를 ApiData 객체로 변환합니다.
+      List<ApiData> data = body
+          .map((dynamic item) => ApiData.fromJson(item))
+          .toList();
+      return data;
+    } else {
+      throw Exception('Failed to load all data');
+    }
+  }
+}
