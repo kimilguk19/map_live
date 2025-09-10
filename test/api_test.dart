a57fe3bed688a3794a03a60b42d1ab94b988e896
kimilguk <kimilguk@knou.ac.kr>
2025-09-10 11:48:18 +0900

    10주차 2교시 강의 소스2

--- test/api_test.dart
+++ test/api_test.dart
@@ -1,10 +1,25 @@
+// test/widget_test.dart
+
 import 'package:flutter_test/flutter_test.dart';
 
+// 테스트할 클래스 또는 함수를 가져옵니다 (예시).
 import 'package:map_live/api_service.dart';
 
 void main() {
-  ApiService apiService = ApiService(); // 객체를 생성 후 메서드를 사용가능하다.
-  test('Api test', () async {
-    await apiService.fetchAllData();
+  group('Counter', () {
+    final apiService = ApiService();
+    test('Api Service', () async {
+      // 실제 앱에서는 여기에 테스트할 클래스의 인스턴스를 생성합니다.
+      // expect(counter.value, 0);
+      await apiService.fetchAllData(); // 이 예제에서는 간단한 단언을 사용합니다.
+    });
+
+    test('value should be incremented', () {
+      // 실제 앱에서는 여기에 테스트할 클래스의 인스턴스를 생성합니다.
+      // final counter = Counter();
+      // counter.increment();
+      // expect(counter.value, 1);
+      expect(1, 1); // 이 예제에서는 간단한 단언을 사용합니다.
+    });
   });
 }
